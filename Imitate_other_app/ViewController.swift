115f91ccaf6b40c0fa5ec45a0b0a75a56631746c
jillou <jilljill860524@gmail.com>
2020-07-12 12:26:31 +0800

    Initial Commit

---
+++ Imitate_other_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Imitate_other_app
+//
+//  Created by JillOU on 2020/7/12.
+//  Copyright © 2020 Jillou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
